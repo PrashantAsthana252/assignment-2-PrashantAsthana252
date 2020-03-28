aaa5d54bf7695ee33300a5fd72f28f87574c33ca
Prashant Asthana <prashant.asthana_cs18@gla.ac.in>
2020-03-28 18:23:42 +0530

    object

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,5 +6,15 @@
  */
 package problem5.main;
 //executable class
+import problem5.circularqueue.MyCircularQueue;
+import problem5.node.Node;
+import problem5.student.Student;
+
+import java.util.Scanner;
+
 public class MyMain {
-}
+    public static void main(String[] args) {
+        MyCircularQueue m = new MyCircularQueue();
+        Node node;
+
+    }
