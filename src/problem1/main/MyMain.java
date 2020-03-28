e78e9e1afa8b4bdddf92bb2b173cb577f041a99c
Prashant Asthana <prashant.asthana_cs18@gla.ac.in>
2020-03-28 09:40:41 +0530

    variable declare

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -5,7 +5,11 @@
  *  Time: 7:17 PM
  */
 package problem1.main;
-
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 
 public abstract class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree mst = new MyBinarySearchTree();
+        TreeNode root;
 }
