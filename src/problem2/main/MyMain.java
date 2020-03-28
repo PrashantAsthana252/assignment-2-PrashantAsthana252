42db7f3273ee95067c54cc98ee3d1f62a91c86e1
Prashant Asthana <prashant.asthana_cs18@gla.ac.in>
2020-03-28 10:59:46 +0530

    object

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -16,5 +16,21 @@ public class MyMain {
         Travarsal t = new Travarsal();
         MyBinarySearchTree mst = new MyBinarySearchTree();
         TreeNode root = new TreeNode(10);
+        mst.insert(root, 5);
+        mst.insert(root, 1);
+        mst.insert(root, 15);
+        mst.insert(root, 9);
+        mst.insert(root, 7);
+        mst.insert(root, 12);
+        mst.insert(root, 30);
+        mst.insert(root, 25);
+        mst.insert(root, 40);
+        mst.insert(root, 45);
+        mst.insert(root, 6);
+        mst.insert(root, 3);
+        t.printPreorder(root);
+        System.out.println();
+        t.printPostorder(root);
 
-}}
+}
+}
--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,10 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+import problem3.myqueue.MyPriorityQueue;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue m = new MyPriorityQueue();
 }
