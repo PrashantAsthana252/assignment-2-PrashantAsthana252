abd4da98262266c2a087a312947ba426245f6692
Prashant Asthana <prashant.asthana_cs18@gla.ac.in>
2020-03-28 18:09:03 +0530

    getter setter

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -26,5 +26,20 @@ public class Student {
 
 
     }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
 
 }
