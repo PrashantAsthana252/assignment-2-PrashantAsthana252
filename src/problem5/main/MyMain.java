2375b0b8c8c711e1179ae1b70ea46e6095ca816f
Prashant Asthana <prashant.asthana_cs18@gla.ac.in>
2020-03-28 18:24:03 +0530

    complete

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -16,5 +16,16 @@ public class MyMain {
     public static void main(String[] args) {
         MyCircularQueue m = new MyCircularQueue();
         Node node;
+        for (int i = 0; i < 5; i++) {
+            node = new Node(new Student());
+            m.enqueue(node);
+        }
+        m.printQueue();
+
+        m.remove(new Scanner(System.in).next());
+        m.printQueue();
+
+        m.process(new Scanner(System.in).next());
+    }
 
     }
